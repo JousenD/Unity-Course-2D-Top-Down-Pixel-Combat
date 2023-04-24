7d442d126166ca62ad7f4552b41d1d9a6fa6ea46
JousenD <romero87_2112@hotmail.com>
2023-04-24 23:51:34 +0200

    8. Sword swing Mirrored

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -6,8 +6,12 @@ public class Sword : MonoBehaviour
 {
     private PlayerControls playerControls;
     private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
     
     private void Awake() {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
         myAnimator = GetComponent<Animator>();
         playerControls = new PlayerControls();
     }
@@ -20,8 +24,25 @@ public class Sword : MonoBehaviour
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
+    private void Update() {
+        MouseFollowWithOffset();
+    }
+
     private void Attack(){
         myAnimator.SetTrigger("Attack");
+    }
+
+    private void MouseFollowWithOffset(){
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x)*Mathf.Rad2Deg;
+
+        if (mousePos.x < playerScreenPoint.x) {
+            activeWeapon.transform.rotation = Quaternion.Euler(0,-180,angle);
+        } else {
+            activeWeapon.transform.rotation = Quaternion.Euler(0,0,angle);
+        }
     }
 
 }
