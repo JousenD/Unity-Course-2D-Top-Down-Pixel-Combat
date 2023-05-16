7c1e6ee322ae66a1863d1d195d3f759466485f6d
JousenD <romero87_2112@hotmail.com>
2023-05-16 18:55:01 +0200

    47. Player Health, Knockback and Flash

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -16,6 +16,7 @@ public class PlayerController : Singleton<PlayerController>
     private Rigidbody2D rb;
     private Animator myAnimator;
     private SpriteRenderer mySpriteRenderer;
+    private Knockback knockback;
     private float startingMoveSpeed;
 
     private bool isDashing = false;
@@ -27,6 +28,7 @@ public class PlayerController : Singleton<PlayerController>
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         mySpriteRenderer = GetComponent<SpriteRenderer>();
+        knockback = GetComponent<Knockback>();
     }
 
     private void Start() {
@@ -60,6 +62,8 @@ public class PlayerController : Singleton<PlayerController>
     }
 
     private void Move(){
+        if (knockback.GettingKnockedBack) {return;}
+
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
 
