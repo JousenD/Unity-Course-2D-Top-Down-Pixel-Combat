587ed0759d45c231b851eb95505211004fde67f3
JousenD <romero87_2112@hotmail.com>
2023-05-23 23:15:07 +0200

    60. Spawn and Loot Pop-Ups

--- Assets/Scripts/Misc/Pickup.cs
+++ Assets/Scripts/Misc/Pickup.cs
@@ -7,12 +7,20 @@ public class Pickup : MonoBehaviour
     [SerializeField] private float pickupDistance = 5f;
     [SerializeField] private float accelerationRate = 0.2f;
     [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private AnimationCurve animCurve;
+    [SerializeField] private float heightY = 1.5f;
+    [SerializeField] private float popDuration = 1f;
+
     private Vector3 moveDir;
     private Rigidbody2D rb;
 
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start() {
+        StartCoroutine(AnimCurveSpawnRoutine());
     }
 
     private void Update() {
@@ -33,6 +41,26 @@ public class Pickup : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.GetComponent<PlayerController>()) {
             Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator AnimCurveSpawnRoutine() {
+        Vector2 startPoint = transform.position;
+        float randomX = transform.position.x +Random.Range(-2f, 2f);
+        float randomY = transform.position.y +Random.Range(-2f, 2f);
+
+        Vector2 endPoint = new Vector2(randomX, randomY);
+
+        float timePassed = 0f;
+
+        while (timePassed < popDuration) {
+            timePassed += Time.deltaTime;
+            float linearT = timePassed / popDuration;
+            float heigthT = animCurve.Evaluate(linearT);
+            float height = Mathf.Lerp(0f, heightY, heigthT);
+
+            transform.position = Vector2.Lerp(startPoint, endPoint, linearT) + new Vector2(0f, height);
+            yield return null;
         }
     }
 }
